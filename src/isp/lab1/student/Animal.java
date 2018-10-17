c8a5660422aa474f91ab0be121ca9954ce72c2b6
smcgovern2 <smcgovern2@my.wctc.edu>
2018-10-17 18:42:34 -0500

    Initial commit

---
+++ src/isp/lab1/student/Animal.java
@@ -0,0 +1,10 @@
+package isp.lab1.student;
+
+public interface Animal {
+    public void talk(String s);
+    public void run(int distance);
+    public void jump();
+    public void hike();
+    public void swim();
+    public void trip();
+}
---
+++ src/isp/lab1/student/Vehicle.java
@@ -0,0 +1,7 @@
+package isp.lab1.student;
+
+public interface Vehicle {
+    public void start();
+    public void drive(int distance);
+    public void stopByBreaking();
+}
